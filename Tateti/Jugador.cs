a0d6da71bee81455eab5079c8cd1decd9e44528c
JeniferB <jenibertona@gmail.com>
2013-06-13 22:03:39 -0300

    Tercer commit de prueba, solo agregando comentarios.

--- Tateti/Jugador.cs
+++ Tateti/Jugador.cs
@@ -5,6 +5,7 @@ namespace tateti
 {
     public class Jugador
     {
+        //declaracion de la propiedad que almacena el tipo de ficha del jugador
         public string tipo_ficha;
         public Hashtable fichas;
 
--- Tateti/Tablero.cs
+++ Tateti/Tablero.cs
@@ -5,8 +5,10 @@ namespace tateti
 {
     public class Tablero
     {
+        //declarando el diccionario hash que manejará las posiciones del tablero.
         public Hashtable posiciones;
 
+        //metodo constructor que inicializara con un valor vacio
         public Tablero()
         {
             posiciones = new Hashtable();
